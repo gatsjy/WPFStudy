0d2009582b792388ad77b86b10036366884b648f
Gatsjy <gkswndks123@naver.com>
2020-12-12 23:20:49 +0900

    2020-12-12, 한주안, 의존프로퍼티 이해하기..

---
+++ SetFontSizeProperty(의존프로퍼티)/SetFontSizeProperty.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace SetFontSizeProperty_의존프로퍼티_
+{
+    class SetFontSizeProperty : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new SetFontSizeProperty());
+        }
+
+        public SetFontSizeProperty()
+        {
+            Title = "Set FontSize Property";
+            SizeToContent = SizeToContent.WidthAndHeight;
+            ResizeMode = ResizeMode.CanMinimize;
+            FontSize = 16;
+            double[] ftnsizes = { 8, 16, 32 };
+
+            // Grid 패널 생성
+            Grid grid = new Grid();
+            Content = grid;
+
+            // 행 과 열 정의
+            for(int i = 0; i <2; i++)
+            {
+                RowDefinition row = new RowDefinition();
+                row.Height = GridLength.Auto;
+                grid.RowDefinitions.Add(row);
+            }
+            for(int i = 0; i < ftnsizes.Length; i++)
+            {
+                ColumnDefinition col = new ColumnDefinition();
+                col.Width = GridLength.Auto;
+                grid.ColumnDefinitions.Add(col);
+            }
+
+            // 6개의 버튼 생성
+            for(int i = 0; i < ftnsizes.Length; i++)
+            {
+                Button btn = new Button();
+                btn.Content = new TextBlock(new Run("Set window FontSize to " + ftnsizes[i]));
+                btn.Tag = ftnsizes[i];
+                btn.HorizontalAlignment = HorizontalAlignment.Center;
+                btn.VerticalAlignment = VerticalAlignment.Center;
+                btn.Click += WindowFontSizeOnClick;
+                grid.Children.Add(btn);
+                Grid.SetRow(btn, 0);
+                Grid.SetColumn(btn, i);
+
+                btn = new Button();
+                btn.Content = new TextBlock(
+                    new Run("Set button FontSize to " + ftnsizes[i]));
+                btn.Tag = ftnsizes[i];
+                btn.HorizontalAlignment = HorizontalAlignment.Center;
+                btn.VerticalAlignment = VerticalAlignment.Center;
+                btn.Click += ButtonFontSizeOnClick;
+                grid.Children.Add(btn);
+                Grid.SetRow(btn, 1);
+                Grid.SetColumn(btn, i);
+            }
+        }
+
+        private void ButtonFontSizeOnClick(object sender, RoutedEventArgs e)
+        {
+            Button btn = e.Source as Button;
+            FontSize = (double)btn.Tag;
+        }
+
+        private void WindowFontSizeOnClick(object sender, RoutedEventArgs e)
+        {
+            Button btn = e.Source as Button;
+            FontSize = (double)btn.Tag;
+        }
+    }
+}
