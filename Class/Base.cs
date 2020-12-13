7683f6cdf08244d57d20c8b0dc2a59472a57f4db
Gatsjy <gkswndks123@naver.com>
2020-12-13 12:24:35 +0900

    2020-12-13, 한주안, Class 관련 공부

---
+++ Class/Base.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class
+{
+    class Base
+    {
+        protected string Name;
+        public Base(string Name)
+        {
+            this.Name = Name;
+            Console.WriteLine($"{this.Name}.Base()");
+        }
+
+        ~Base()
+        {
+            Console.WriteLine($"~Base({this.Name}.~Base()");
+        }
+
+        public void BaseMethod()
+        {
+            Console.WriteLine($"{Name}.BaseMethod()");
+        }
+    }
+
+    class Derived : Base
+    {
+        public Derived(string Name) : base(Name)
+        {
+            Console.WriteLine($"{this.Name}.Derived()");
+        }
+
+        ~Derived()
+        {
+            Console.WriteLine($"{this.Name}.~Derived()");
+        }
+
+        public void DerivedMethod()
+        {
+            Console.WriteLine($"{Name}.DerivedMethod()");
+        }
+    }
+}
---
+++ Class/MainApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            //Base a = new Base("a");
+            //a.BaseMethod();
+
+            Derived b = new Derived("b");
+            b.BaseMethod();
+            //b.DerivedMethod();
+        }
+    }
+}
