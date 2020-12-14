23441297d4c50c501fe3c99fe4928b08a3299104
Gatsjy <gkswndks123@naver.com>
2020-12-14 14:13:47 +0900

    2020-12-14, 한주안, 람다심 관련 공부

---
+++ ActionTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ActionTest
+{
+    class Program
+    {
+        // 14.4.2 Action 대리자
+        // Action 대리자는 Func 대리자와 거의 똑같다. 차이점이라면 Action 대리자는 반환형식이 없다는 것 뿐이다.
+        // Func와 달리 어떤 결과를 반환하는 것을 목적으로 하지 않고, 일련의 작업을 수행하는 것이 목적이다.
+        static void Main(string[] args)
+        {
+            Action act1 = () => Console.WriteLine("Action()");
+            act1();
+
+            int result = 0;
+            Action<int> act2 = (x) => result = x * x;
+
+            act2(3);
+            Console.WriteLine($"result : {result}");
+
+            Action<double, double> act3 = (x, y) =>
+            {
+                double pi = x / y;
+                Console.WriteLine($"Action<T1, T2>({x},{y}) : {pi}");
+            };
+            act3(22.0, 7.0);
+        }
+    }
+}
---
+++ FuncTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace FuncTest
+{
+    // Func와 Action으로 더 간편하기 무명 함수 만들기
+    // 익명메소드와 무명함수는 코드를 보다 간결하게 만들어주는 요소들입니다. 하지만 이들을 선언하기 전에 해야하는 작업을 생각해보라..
+    // 대부분의 경우 단 하나의 익명 메소드나 무명함수를 만들기 위해 별개의 대리자를 선언해야한다..
+    // Func 대리자는 결과를 반환하는 메소드를 참조하기 위해서
+    // Action 대리자는 결과를 반환하지 않는 메소드를 참조합니다.
+    class Program
+    {
+        // 14.4.1 Func 대리자
+        static void Main(string[] args)
+        {
+            Func<int> func1 = () => 10;
+            Console.WriteLine($"func1() : {func1()}");
+
+            Func<int, int> func2 = (x) => x * 2;
+            Console.WriteLine($"func2(4) : {func2(4)}");
+
+            Func<double, double, double> func3 = (x, y)=>x / y;
+            Console.WriteLine($"func3(22,7) : {func3(22, 7)}");
+        }
+    }
+}
