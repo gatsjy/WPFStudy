966a435dfddc48c05ad7ff8e320ea1b09359cf61
Gatsjy <gkswndks123@naver.com>
2020-12-20 19:05:19 +0900

    2020-12-20, 한주안, 템플릿 관련 공부

---
+++ ContentTemplateDemo/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentTemplateDemo
+{
+    public class Employee
+    {
+        // Private 필드
+        string name;
+        string face;
+        DateTime birthdate;
+        bool Lefthanded;
+
+        // XAML에서 사용되는 인자 없는 생성자
+        public Employee()
+        {
+        }
+
+        // C# 코드에서 사용되는 인자를 가진 생성자
+        public Employee(string name, string face, DateTime birthdate, bool lefthanded)
+        {
+            Name = name;
+            Face = face;
+            BirthDate = birthdate;
+            Lefthanded = lefthanded;
+        }
+
+        // public 프로퍼티
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        public string Face
+        {
+            set { face = value; }
+            get { return face; }
+        }
+        public DateTime BirthDate
+        {
+            set { birthdate = value; }
+            get { return birthdate; }
+        }
+        public bool LeftHanded
+        {
+            set { LeftHanded = value; }
+            get { return Lefthanded; }
+        }
+    }
+}
---
+++ ContentTemplateDemo/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContentTemplateDemo
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // 새로운 EmplyeeButton을 추가하는 코드를 보여줌
+            EmployeeButton btn = new EmployeeButton();
+            btn.Content = new Employee("Jim", "Jim.png", new DateTime(1975, 6, 15), false);
+            stack.Children.Add(btn);
+        }
+
+        // 버튼의 Click 이벤트 핸들러
+        private void EmplyeeButtonClick(object sender, RoutedEventArgs args)
+        {
+            Button btn = args.Source as Button;
+            Employee emp = btn.Content as Employee;
+            MessageBox.Show(emp.Name + " button clicked!", Title);
+        }
+    }
+}
