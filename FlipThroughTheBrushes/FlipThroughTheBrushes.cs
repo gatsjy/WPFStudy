c25db82d26323ef98210245981abb63c17545115
Gatsjy <gkswndks123@naver.com>
2021-04-08 22:49:49 +0900

    2021-04-08, 한주안, wpf 브러쉬

---
+++ FlipThroughTheBrushes/FlipThroughTheBrushes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FlipThroughTheBrushes
+{
+    class FlipThroughTheBrushes : Window
+    {
+        int index = 0;
+        PropertyInfo[] repos;
+
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new FlipThroughTheBrushes());
+        }
+    }
+}
