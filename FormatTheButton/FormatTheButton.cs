2d52dbea86a5ee348222ed249dcc55007d565441
Gatsjy <gkswndks123@naver.com>
2021-04-13 21:51:09 +0900

    2021-04-13, 한주안, 버튼과 컨트롤

---
+++ FormatTheButton/FormatTheButton.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace FormatTheButton
+{
+    class FormatTheButton : Window
+    {
+        Run runButton;
+
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new FormatTheButton());
+        }
+        public FormatTheButton()
+        {
+            Title = "Format the Button";
+
+            // 버튼을 생성하고 Window의 Content로 설정
+            Button btn = new Button();
+            btn.HorizontalAlignment = HorizontalAlignment.Center;
+            btn.VerticalAlignment = VerticalAlignment.Center;
+            btn.MouseEnter += ButtonOnMouseEnter;
+            btn.MouseLeave += ButtonOnMouseLeave;
+            Content = btn;
+
+            // TextBlock을 생성하고 버튼의 Content로 설정
+            TextBlock txtblk = new TextBlock();
+            txtblk.FontSize = 24;
+            txtblk.TextAlignment = TextAlignment.Center;
+            btn.Content = txtblk;
+
+            // TextBlock에 서식화된 텍스트를 추가
+            txtblk.Inlines.Add(new Italic(new Run("Click")));
+            txtblk.Inlines.Add(" the ");
+            txtblk.Inlines.Add(runButton = new Run("button"));
+            txtblk.Inlines.Add(new LineBreak());
+            txtblk.Inlines.Add("to launch the ");
+            txtblk.Inlines.Add(new Bold(new Run("rocket")));
+        }
+
+        private void ButtonOnMouseLeave(object sender, MouseEventArgs e)
+        {
+            runButton.Foreground = Brushes.Red;
+        }
+
+        private void ButtonOnMouseEnter(object sender, MouseEventArgs e)
+        {
+            runButton.Foreground = SystemColors.ControlTextBrush;
+        }
+    }
+}
---
+++ RenderTehGraphic/RenderTheGraphic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Shapes;
+
+namespace RenderTehGraphic
+{
+    class RenderTheGraphic : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new RenderTheGraphic());
+        }
+        public RenderTheGraphic()
+        {
+            Title = "Render the Graphic";
+            SimpleEllipse ellipse = new SimpleEllipse();
+            Content = ellipse;
+        }
+    }
+}
