b54d9f093db7385dc1d74b0a019396123ba90bf1
Gatsjy <gkswndks123@naver.com>
2021-04-14 20:54:25 +0900

    2021-04-14, 한주안, 버튼과 컨트롤2

---
+++ CommandTheButton/CommandButton.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CommandTheButton
+{
+    class CommandTheButton : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new CommandTheButton());
+        }
+        public CommandTheButton()
+        {
+            Title = "Command the Button";
+
+            // 버튼을 생성하고, Window의 컨텐트로 설정
+            Button btn = new Button();
+            btn.HorizontalAlignment = HorizontalAlignment.Center;
+            btn.VerticalAlignment = VerticalAlignment.Center;
+            btn.Command = ApplicationCommands.Paste;
+            btn.Content = ApplicationCommands.Paste.Text;
+            Content = btn;
+
+            // Command와 이벤트 핸들러의 바인딩
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Paste, PasteOnExecute, PasteCanExecute));
+        }
+
+        private void PasteCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = Clipboard.ContainsText();
+        }
+
+        private void PasteOnExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            Title = Clipboard.GetText();
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(e);
+            Title = "Command the Button";
+        }
+    }
+}
