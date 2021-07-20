ea77f0c061af85722c461ec1c9ab2d2bb6bde81a
Gatsjy <gkswndks123@naver.com>
2021-07-20 22:49:26 +0900

    2021-07-20, 한주안, mvvm 예제1

---
+++ mvvmExample/Model/Model.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvvmExample
+{
+    class Model : INotifyPropertyChanged
+    {
+        private string NAME_;
+
+        public string NAME
+        {
+            get
+            {
+                return this.NAME_;
+            }
+            set
+            {
+                this.NAME_ = value;
+                this.OnPropertyChanged("NAME");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            if(PropertyChanged != null)
+            {
+                try
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(name));
+                }
+                catch
+                {
+                }
+            }
+        }
+        
+        public Model()
+        {
+        }
+    }
+}
---
+++ mvvmExample/Model/ModelView.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvvmExample
+{
+    class ModelView : INotifyPropertyChanged
+    {
+        public RelayCommand AddCommand { get; set; }
+
+        private ObservableCollection<Model> ROOT_;
+
+        public ObservableCollection<Model> ROOT
+        {
+            get
+            {
+                return this.ROOT_;
+            }
+            set
+            {
+                this.ROOT_ = value;
+                this.OnPropertyChanged("RCP");
+            }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            if(PropertyChanged != null)
+            {
+                try
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(name));
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        public ModelView()
+        {
+            this.ROOT = new ObservableCollection<Model>();
+            this.AddCommand = new RelayCommand(AddExecuted, AddCanExecuted); //바인딩을 시켜준다.
+        }
+        
+        private bool AddCanExecuted(object param)
+        {
+            bool ret = false;
+            ret = true;
+            return ret;
+        }
+
+        private void AddExecuted(object param)
+        {
+            this.ROOT.Add(new Model() { NAME = "TEST" });
+        }
+    }
+}
