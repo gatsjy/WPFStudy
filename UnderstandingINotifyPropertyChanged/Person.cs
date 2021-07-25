8e4126f55930c56c6e3effee9ca41b8032b4b475
Gatsjy <gkswndks123@naver.com>
2021-07-25 22:58:38 +0900

    2021-07-25, 한주안, inotifiedChanged 공부 및 이벤트에 대한 조금의 이해..

---
+++ UnderstandingINotifyPropertyChanged/Person.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingINotifyPropertyChanged
+{
+    class Person : INotifyPropertyChanged
+    {
+        private string firstName;
+        private string lastName;
+        private string fullName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (firstName != value)
+                {
+                    firstName = value;
+                    OnPropertyChanged("FirstName");
+                    OnPropertyChanged("FullName");
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (lastName != value)
+                {
+                    lastName = value;
+                    OnPropertyChanged("LastName");
+                    OnPropertyChanged("FullName");
+                }
+            }
+        }
+
+        public string FullName
+        {
+            get { return fullName = firstName + " " + lastName; }
+            set
+            {
+                if (fullName != value)
+                {
+                    fullName = value;
+                    OnPropertyChanged("FullName");
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
