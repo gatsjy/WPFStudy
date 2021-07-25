4a3186bf0a95c2f2cc4080f94c91ba8b45ee5e73
Gatsjy <gkswndks123@naver.com>
2021-07-25 21:32:35 +0900

    2021-07-25, 한주안, DataTemplates 관련 실습

---
+++ DataTemplatesDemo/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataTemplatesDemo
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Person obj { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            obj = new Person();
+
+            obj.Name = "hanjuan";
+
+            this.DataContext = this;
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return "바보" + Name;
+        }
+    }
+}
