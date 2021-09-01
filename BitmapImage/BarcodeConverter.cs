2c623fabfb65d6311d9569af3903f318755add6b
Gatsjy <gkswndks123@naver.com>
2021-09-01 23:30:18 +0900

    2021-09-01, 한주안, 바코드 관련 공부

---
+++ BitmapImage/BarcodeConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using TECIT.TBarCode;
+
+namespace BitmapImageDemo
+{
+    class BarcodeConverter : System.Windows.Data.IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string inputStr = value as string;
+            BitmapImage bi = new BitmapImage();
+
+            if (value == null) return bi;
+            if (string.IsNullOrEmpty(inputStr)) return bi;
+
+            string returnOutputStr = inputStr.ToUpper();
+            Barcode barcode = new Barcode();
+            barcode.Data = returnOutputStr;
+            barcode.BarcodeType = BarcodeType.Code39;
+            barcode.Dpi = 100;
+            barcode.SizeMode = SizeMode.FitToBoundingRectangle;
+            barcode.TextClipping = false;
+            barcode.IsTextVisible = false; // 하단에 텍스트가 나올 것인지 안나올 것인지..
+            System.Drawing.Size optimalSize = barcode.CalculateOptimalBitmapSize(null, 1, 1);
+            barcode.BoundingRectangle = new System.Drawing.Rectangle(0, 0, optimalSize.Width, optimalSize.Height);
+
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+            {
+                Bitmap barcodeBitmap = barcode.DrawBitmap();
+                barcodeBitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+                ms.Position = 0;
+                BitmapImage returnBi = new BitmapImage();
+                returnBi.BeginInit();
+                returnBi.StreamSource = ms;
+                returnBi.CacheOption = BitmapCacheOption.OnLoad;
+                returnBi.EndInit();
+                return returnBi;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BitmapImage/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitmapImageDemo
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string pt_no = "12345676";
+        public string PT_NO
+        {
+            get => pt_no;
+            set
+            {
+                pt_no = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
