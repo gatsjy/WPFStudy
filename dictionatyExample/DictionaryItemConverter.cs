eb7fac47b99c93e64e3393f033fbf81e6023c65b
Gatsjy <gkswndks123@naver.com>
2021-11-08 22:51:01 +0900

    2021-11-08, 한주안, dictionary converter로 세팅

---
+++ dictionatyExample/DictionaryItemConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace dictionatyExample
+{
+    class DictionaryItemConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var dict = value as Dictionary<string, string>;
+            if(dict != null)
+            {
+                return dict[parameter as string];
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
